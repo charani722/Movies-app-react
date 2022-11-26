edf3a0b547ee91163ca2e69d7cf8a32a04cd1e4d
Charani <sricharani.nukala@gmail.com>
2022-11-26 18:20:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useState, useEffect} from "react";
+
+
+
+let allMovies = [];
+
+
+const API_URL = "https://www.omdbapi.com/?apikey=45f0782a&s=war"
 
 function App() {
+
+  const [movies, setMovies] = useState(null)
+  const [error, setError] = useState(null)
+ 
+  useEffect(() => {
+    axios.get(API_URL).then(response => {
+      allMovies = response.data.Search
+      setMovies(response.data.Search)
+      console.log(response)
+    }).catch(err => setError(err.message))
+  }, [])
+
+  const resultMovies = (searchValue) => {
+    if (movies) {
+      setMovies(allMovies.filter(movie => movie.Title.toLowerCase().includes(searchValue)))
+    }
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input type="text" placeholder='Search for Movie here'onChange={(e) => resultMovies(e.target.value.toLowerCase())} />
+      <div className='movie-container'> 
+      
+      {movies ? movies.map(movie => <div className="movie-card">
+          <img src={movie.Poster}/>
+          <p >{movie.Title}</p>
+          </div>)
+        :<div className='movieDiv'> NO Movies found </div>
+        }
+       
+        
+    
+    
+     </div>
     </div>
   );
 }
